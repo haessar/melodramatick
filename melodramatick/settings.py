534ff2af87697d660c3406fe7ddbf21018241de8
Will Haese-Hill <wahhill@gmail.com>
2023-07-24 10:44:58 +0100

    add default console logging

--- melodramatick/settings.py
+++ melodramatick/settings.py
@@ -201,6 +201,30 @@ else:
     }
 
 
+# Logging
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "WARNING",
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": os.getenv("DJANGO_LOG_LEVEL", "INFO"),
+            "propagate": False,
+        },
+    },
+}
+
+
 # Site-specific settings
 
 SITE = __package__.title()
