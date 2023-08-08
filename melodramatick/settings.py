7ae9e69d7cce7477e8db4d601be8b98b76f70b5f
Will Haese-Hill <wahhill@gmail.com>
2023-08-08 12:45:02 +0100

    tests for melodramatick.composer

--- melodramatick/settings.py
+++ melodramatick/settings.py
@@ -124,6 +124,9 @@ if DEVELOPMENT_MODE is True:
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
+            'TEST': {
+                "NAME": "test_db"
+            }
         }
     }
 elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
