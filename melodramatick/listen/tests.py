d14e3f048f936a6740b76aedd08f4c29e0780781
Will Haese-Hill <wahhill@gmail.com>
2023-08-09 11:06:21 +0100

    listen and top_list view test cases

--- melodramatick/listen/tests.py
+++ melodramatick/listen/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Listen
+from .views import spotify_playback_view
+from melodramatick.accounts.models import CustomUser
+from melodramatick.work.models import Work
+
+
+class SpotifyPlaybackViewTestCase(TestCase):
+    fixtures = ["composer.json", "sites.json", "user.json", "work.json"]
+
+    def setUp(self):
+        response = self.client.get("/")
+        self.request = response.wsgi_request
+        self.request.META['HTTP_REFERER'] = "/"
+        self.request.user = CustomUser.objects.get(id=1)
+
+    def test_spotify_playback_view(self):
+        work_id = 230 if self.request.site.id == 1 else 722
+        spotify_playback_view(self.request, work=work_id)
+        spotify_playback_view(self.request, work=work_id)
+        qs = Listen.objects.all()
+        # Only 1 listen object should be created...
+        self.assertEqual(len(qs), 1)
+        listen = qs.first()
+        # ...with tally of 2
+        self.assertEqual(listen.tally, 2)
+        self.assertEqual(listen.user, self.request.user)
+        work = Work.objects.get(id=work_id)
+        self.assertEqual(listen.work, work)
--- melodramatick/top_list/tests.py
+++ melodramatick/top_list/tests.py
@@ -1,3 +1,25 @@
-# from django.test import TestCase
+from django.test import TestCase
 
-# Create your tests here.
+from .views import TopListView
+
+
+class TopListViewTestCase(TestCase):
+    fixtures = ["composer.json", "sites.json", "top_list.json", "user.json", "work.json"]
+
+    def setUp(self):
+        response = self.client.get("/")
+        self.request = response.wsgi_request
+        self.view = TopListView()
+        self.view.setup(self.request)
+
+    def test_get_table_data(self):
+        table_data = self.view.get_table_data()
+        if self.request.site.id == 1:
+            self.assertListEqual(
+                list(table_data.values('max_position', 'list_work_count').order_by('id')),
+                [{'max_position': 2, 'list_work_count': 2},  # <List: Top operas - Example Dot Com>
+                 {'max_position': 1, 'list_work_count': 1}]  # <List: Another List - Example Dot Com>
+            )
+        elif self.request.site.id == 2:
+            # TODO - Write top_list fixtures for balletick
+            pass
