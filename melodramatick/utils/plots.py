31e3a07a20c53828b039ace9eaa7956b66725d88
Will Haese-Hill <wahhill@gmail.com>
2023-11-15 12:14:28 +0000

    catch empty figure

--- melodramatick/utils/plots.py
+++ melodramatick/utils/plots.py
@@ -4,13 +4,20 @@ import io
 import matplotlib.pyplot as plt
 
 
+class EmptyFigure(Exception):
+    pass
+
+
 def to_bytes_fig(f):
     """
     Decorator to convert plot to bytes object
     """
     def wrapped(*args, **kwargs):
         fig, ax = plt.subplots(figsize=kwargs.pop("figsize", (8, 4)))
-        f(ax, *args, **kwargs)
+        try:
+            f(ax, *args, **kwargs)
+        except EmptyFigure:
+            return
         flike = io.BytesIO()
         fig.savefig(flike, bbox_inches="tight")
         return base64.b64encode(flike.getvalue()).decode()
