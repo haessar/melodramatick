bc4a81e99a921400df13343c98f1d5633121505f
Will Haese-Hill <wahhill@gmail.com>
2023-11-16 21:41:55 +0000

    added most watched company card,  and tidied code

--- melodramatick/accounts/tests.py
+++ melodramatick/accounts/tests.py
@@ -12,9 +12,12 @@ class ProfileViewTestCase(TestCase):
     def test_get_context_data(self):
         # TODO Add tests for context["progress"] and context["awards"]
         if self.response.wsgi_request.site.id == 1:
-            self.assertEqual(self.response.context_data['most_listened'].tally, 2)
             self.assertEqual(len(self.response.context_data['listens']), 2)
-            self.assertEqual(self.response.context_data['most_visited'].venue_tally, 2)
+            self.assertEqual(self.response.context_data['most_listened'].tally, 2)
+            self.assertEqual(len(self.response.context_data['performances']), 3)
+            self.assertEqual(self.response.context_data['most_watched'].tally, 2)
+            self.assertEqual(self.response.context_data['most_visited'].tally, 2)
+            self.assertEqual(self.response.context_data['most_watched_company'].tally, 3)
         elif self.response.wsgi_request.site.id == 2:
             self.assertEqual(self.response.context_data['most_listened'].tally, 3)
             self.assertEqual(len(self.response.context_data['listens']), 1)
--- melodramatick/accounts/views.py
+++ melodramatick/accounts/views.py
@@ -6,7 +6,7 @@ from django.views.generic.edit import CreateView
 from .forms import CustomUserCreationForm
 from .models import CustomUser
 from melodramatick.listen.models import Listen
-from melodramatick.performance.models import Performance, Venue
+from melodramatick.performance.models import Company, Performance, Venue
 from melodramatick.performance.plots import plot_perfs_per_year
 from melodramatick.work.models import Work
 
@@ -23,26 +23,43 @@ class ProfileView(DetailView):
     slug_url_kwarg = "username"
     template_name = 'accounts/profile.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        user = self.get_object()
+    def _set_awards_tab_context_data(self, context):
         context["progress"] = sorted(
-            user.progress.filter(count__gt=0).filter(list__site=self.request.site),
+            self.user.progress.filter(count__gt=0).filter(list__site=self.request.site),
             key=lambda x: x.ticks_to_next_award)
         context["awards"] = sorted(
-            user.award.filter(list__site=self.request.site),
+            self.user.award.filter(list__site=self.request.site),
             key=lambda x: (x.level.rank, -x.list.length))
-        context["listens"] = Listen.objects.filter(user=user)
-        context["performances"] = Performance.objects.filter(user=user).order_by('-date')
+
+    def _set_performances_tab_context_data(self, context):
+        context["performances"] = Performance.objects.filter(user=self.user).order_by('-date')
+        context["most_watched"] = Work.objects.annotate(
+            tally=Count("performance", filter=Q(
+                performance__user=self.user,
+                performance__site=self.request.site))
+            ).order_by("-tally").first()
+        context["most_visited"] = Venue.objects.annotate(
+            tally=Count("performance", filter=Q(
+                performance__user=self.user,
+                performance__streamed=False,
+                performance__site=self.request.site))
+            ).order_by("-tally").first()
+        context["most_watched_company"] = Company.objects.annotate(
+            tally=Count("performance", filter=Q(
+                performance__user=self.user,
+                performance__site=self.request.site))
+            ).order_by("-tally").first()
+        context["perfs_per_year"] = plot_perfs_per_year(context["performances"], figsize=(6, 6))
+
+    def _set_listens_tab_context_data(self, context):
+        context["listens"] = Listen.objects.filter(user=self.user)
         max_tally = context["listens"].aggregate(Max("tally"))["tally__max"]
         context["most_listened"] = context["listens"].filter(tally=max_tally).first()
-        works_with_counts = Work.objects.filter(performance__in=context['performances']).annotate(Count('performance'))
-        context["most_watched"] = works_with_counts.order_by('-performance__count').first()
-        venues = Venue.objects.annotate(
-            venue_tally=Count("performance", filter=Q(
-                performance__user=user,
-                performance__streamed=False,
-                performance__site=self.request.site)))
-        context["most_visited"] = venues.order_by('-venue_tally').first()
-        context["perfs_per_year"] = plot_perfs_per_year(context["performances"], figsize=(6, 6))
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        self.user = self.get_object()
+        self._set_awards_tab_context_data(context)
+        self._set_performances_tab_context_data(context)
+        self._set_listens_tab_context_data(context)
         return context
