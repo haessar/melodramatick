44dfd70920f43937cce930346d697f7f237c28dc
Will Haese-Hill <wahhill@gmail.com>
2023-11-17 14:05:22 +0000

    reflect Count change in attributes and yaxis label

--- melodramatick/listen/plots.py
+++ melodramatick/listen/plots.py
@@ -21,19 +21,19 @@ def plot_listens_per_week(ax, qs):
         all_weeks_df = pd.DataFrame(index=week_idx)
         # "Count" the number of listens, rather than "Sum" the tally. This is because a Listen that is updated
         # with an increment to tally X will not necessarily have been listened to X times that week.
-        df = pd.DataFrame(qs.values('updated_at__week', 'updated_at__year').annotate(tally=Count('tally')))
+        df = pd.DataFrame(qs.values('updated_at__week', 'updated_at__year').annotate(count=Count('tally')))
         df = df.groupby(['updated_at__week', 'updated_at__year']).sum().reset_index()
         # Determine start date of week (from Monday)
         df['week'] = (
             df['updated_at__week'].astype(str) + '-' + df['updated_at__year'].astype(str)
             ).apply(lambda x: datetime.strptime(x + '-1', '%W-%Y-%w').strftime(DATE_FORMAT))
-        df = pd.concat([all_weeks_df, df.set_index('week')[['tally']]], axis=1)
+        df = pd.concat([all_weeks_df, df.set_index('week')[['count']]], axis=1)
 
         df.plot.bar(ax=ax, color=settings.BACKGROUND_COLOUR)
 
         ax.set_title('Listens per week (past {})'.format(WEEKS_AGO))
         ax.yaxis.grid(b=True, which='major', color='black', linestyle='-')
-        ax.set_ylabel("Tally")
+        ax.set_ylabel("Count")
         ax.set_xlabel("Week beginning")
         ax.set_xticklabels(df.index, rotation=45, ha="right", rotation_mode='anchor')
         ax.get_legend().remove()
