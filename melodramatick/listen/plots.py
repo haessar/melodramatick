21345e291e84a38413001beb4835a22676d01b91
Will Haese-Hill <wahhill@gmail.com>
2023-11-17 13:53:17 +0000

    convert to listens_per_week

--- melodramatick/listen/plots.py
+++ melodramatick/listen/plots.py
@@ -1,30 +1,39 @@
 from datetime import datetime, timedelta
 
 from django.conf import settings
-from matplotlib.ticker import MultipleLocator
+from django.db.models import Sum
 import pandas as pd
 
 from melodramatick.utils.plots import EmptyFigure, to_bytes_fig
 
 
+WEEKS_AGO = 16
+DATE_FORMAT = "%Y-%m-%d"
+
+
 @to_bytes_fig
-def plot_listens_per_month(ax, qs):
-    qs = qs.exclude(updated_at__isnull=True).exclude(updated_at__lte=datetime.now()-timedelta(weeks=52))
+def plot_listens_per_week(ax, qs):
+    qs = qs.exclude(updated_at__isnull=True).exclude(updated_at__lte=datetime.now()-timedelta(weeks=WEEKS_AGO))
     if qs:
-        if "sqlite3" in settings.DATABASES['default']['ENGINE']:
-            months = qs.extra(select={'month': "strftime('%%m', updated_at)"}).values('month', 'tally')
-        else:
-            months = qs.extra(select={'month': "DATE_FORMAT(updated_at, '%%m')"}).values('month', 'tally')
-        df = pd.DataFrame(months).groupby('month').sum()
+        week_idx = pd.date_range(
+            start=datetime.now()-timedelta(weeks=WEEKS_AGO),
+            end=datetime.now(), freq="W-MON").strftime(DATE_FORMAT)
+        all_weeks_df = pd.DataFrame(index=week_idx)
+        df = pd.DataFrame(qs.values('updated_at__week', 'updated_at__year').annotate(tally=Sum('tally')))
+        df = df.groupby(['updated_at__week', 'updated_at__year']).sum().reset_index()
+        # Determine start date of week (from Monday)
+        df['week'] = (
+            df['updated_at__week'].astype(str) + '-' + df['updated_at__year'].astype(str)
+            ).apply(lambda x: datetime.strptime(x + '-1', '%W-%Y-%w').strftime(DATE_FORMAT))
+        df = pd.concat([all_weeks_df, df.set_index('week')[['tally']]], axis=1)
 
-        df.plot.bar(ax=ax, color=settings.BACKGROUND_COLOUR, rot=45)
+        df.plot.bar(ax=ax, color=settings.BACKGROUND_COLOUR)
 
-        ax.set_title('Listens per month')
-        ax.yaxis.set_minor_locator(MultipleLocator(1))
+        ax.set_title('Listens per week (past {})'.format(WEEKS_AGO))
         ax.yaxis.grid(b=True, which='major', color='black', linestyle='-')
-        ax.yaxis.grid(b=True, which='minor', linestyle='--')
         ax.set_ylabel("Tally")
-        ax.set_xlabel("Month")
+        ax.set_xlabel("Week beginning")
+        ax.set_xticklabels(df.index, rotation=45, ha="right", rotation_mode='anchor')
         ax.get_legend().remove()
     else:
         raise EmptyFigure
