ee4bc32e03b09362d1d840941088ef86599f76cc
Will Haese-Hill <wahhill@gmail.com>
2024-02-27 21:21:26 +0000

    app_template for quick starting new melodramatick apps

---
+++ melodramatick/management/commands/startapptick.py
@@ -0,0 +1,26 @@
+from django.core.management.templates import TemplateCommand
+from django.conf import settings
+
+
+class Command(TemplateCommand):
+    help = (
+        "Creates a Django app directory structure for the given app name in "
+        "the current directory or optionally in the given directory."
+    )
+    missing_args_message = "You must provide an application name."
+
+    def handle(self, **options):
+        app_name = options.pop("name")
+        target = options.pop("directory")
+        super().handle("app", app_name, target, **options)
+
+    def add_arguments(self, parser):
+        super().add_arguments(parser)
+        parser.add_argument(
+            "--work",
+            help="Name of core work model for app.",
+        )
+        parser.add_argument(
+            "--colour-hex",
+            help="Colour theme for app. e.g. '#ecdff2'"
+        )
